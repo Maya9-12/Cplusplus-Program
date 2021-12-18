83ddeeb21b250a07474c37a7b623a48ffe92fdab
Maya9-12 <95621084+Maya9-12@users.noreply.github.com>
2021-12-18 03:09:31 -0500

    Add files via upload

---
+++ PythonCode.py
@@ -0,0 +1,14 @@
+import re
+import string
+
+def MultiplicationTable(v:str):
+        v = int(v)
+        #Loop to print table
+        for i in range(1, 11):
+                print(v, "X" , i, "=", v * i, sep = ' ')
+        return 1
+def DoubleValue(v):
+    return v * 2
+
+
+    
